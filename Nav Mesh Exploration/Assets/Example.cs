daab4fa11317a7110cbc3ae332a39e2d65501cb8
AugustusWendell <augustuswendell@gmail.com>
2018-06-07 01:09:38 -0400

    060718_am

--- Nav Mesh Exploration/Assets/Example.cs
+++ Nav Mesh Exploration/Assets/Example.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class Example : MonoBehaviour
 {
     //Make sure to attach these Buttons in the Inspector
-    public Button m_Target1Button, m_Target2Button;
+    public Button m_Target1Button, m_Target2Button, m_AddAgentButton;
     public GameObject controller;
     public GameObject target1;
     public GameObject target2;
     public NavMeshAgent Agent1;
     public NavMeshAgent Agent2;
+    public GameObject SSAgent;
+    //private List AgentList;
 
     void Start()
     {
@@ -25,12 +30,15 @@ public class Example : MonoBehaviour
 
         m_Target1Button.onClick.AddListener(delegate { UpdateTarget1(); });
         m_Target2Button.onClick.AddListener(delegate { UpdateTarget2(); });
+        m_AddAgentButton.onClick.AddListener(delegate { AddAgent(); });
 
 
         //set/populate controller and target objects
         controller = GameObject.Find("Controller");
         target1 = GameObject.Find("Target 1");
         target2 = GameObject.Find("Target 2");
+
+        List<NavMeshAgent> AgentList = new List<NavMeshAgent>();
     }
 
     void TaskOnClick()
@@ -51,8 +59,14 @@ public class Example : MonoBehaviour
         //set all agents to go to target 1
         Agent1.SetDestination(target1.transform.position);
         Agent2.SetDestination(target1.transform.position);
-
 	}
+
+    void AddAgent()
+    {
+        GameObject newAgent = (GameObject)Instantiate(SSAgent, transform.position, transform.rotation);
+        AgentList.add(newAgent);
+    }
+
 
     void UpdateTarget2()
     {
